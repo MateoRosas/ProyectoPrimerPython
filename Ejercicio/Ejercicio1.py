4d2c0ca7b89a781a17f4dad60d690e179fa3076c
Mateo Rosas <mateosrosasm@outlook.es>
2020-09-07 09:03:18 -0500

    Tercer commit, desarrollo Ejercicio3 07/09/2020

---
+++ Ejercicio/Ejercicio1.py
@@ -0,0 +1,6 @@
+print("Bienvenido, Este programa le ayudará a saber si un número es par o impar")
+numeroEntra = int (input("Digite cualquier numero Real: "))
+if numeroEntra % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
