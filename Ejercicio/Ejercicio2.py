57f513d3cd06bad53eb227244a083798a45d5d15
Mateo Rosas <mateosrosasm@outlook.es>
2020-09-08 23:58:54 -0500

    Tercer commit tercer programa 08/09/2020

---
+++ Ejercicio/Ejercicio2.py
@@ -0,0 +1,8 @@
+print("Este programa le permitirá saber cuantos años tendrá en el 2070")
+edad = (int(input("Ingrese su edad actual: ")))
+fechaActual = (int(input("Ingrese el año actual: ")))
+
+resultado = (2070-fechaActual)
+resultadoFinal = (resultado+edad)
+
+print("Su edad en el año 2070 será de: ", resultadoFinal, "Años")
